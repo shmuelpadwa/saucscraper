2c3833fbb3bf422ff25afefbc6a5da79eeb9a47c
Shmu <padwas@bxscience.edu>
2020-07-18 00:27:24 -0400

    default centering to P

--- scrape.vb
+++ scrape.vb
@@ -28,7 +28,7 @@ Sub Scrape()
     gammaangle = "90"
     myMetric = "S6" 'In quotes, put S6, L1, L2, NCDist, V7, or D7
     spheresort = "f" 'In quotes, put f or family or d or distance
-    centering = "B" 'In quotes, put lattice centering as P, A, B, C, F, I, R, H, V
+    centering = "P" 'In quotes, put lattice centering as P, A, B, C, F, I, R, H, V
     pora = "p" 'Percent or Angstroms. Put p or a or P or A
     proteincolumn = "A" 'Put column of excel sheet you want to have protein names. Shift over by two to run a different metric on the same numbers.
     distcolumn = "B" 'Put column of excel sheet you want to have distances. Shift over by two to run a different metric on the same numbers.
